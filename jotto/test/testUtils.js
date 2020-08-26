4153883b7981c31050dea5b6ccfdcdf6c659d132
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-26 16:50:55 +0530

    add thunk for guessedwords piece of state

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,10 +1,12 @@
 import checkPropTypes from 'check-prop-types'
 import { Component } from 'react'
 import rootReducer from '../src/reducers';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { middlewares } from '../src/configureStore';
 
 export const storeFactory=(initialState)=>{
-    return createStore(rootReducer, initialState);
+    const createStoreWithMiddleware=applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 }
 export const findByTestAttr=(wrapper,val)=>{
     return wrapper.find(`[data-test="${val}"]`)
