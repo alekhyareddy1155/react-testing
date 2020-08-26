3224ea61f272f40aec37768c7a69e057b0f6281b
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-26 10:37:28 +0530

    setup guessedwords test

---
+++ jotto/src/integration.test.js
@@ -0,0 +1,20 @@
+import { storeFactory } from '../test/testUtils';
+import { guessedWord } from './actions';
+describe('guessword action dispatcher',()=>{
+    describe('no guessed words',()=>{
+        test('updates state correctly for unsuccessful guess',()=>{
+
+        })
+        test('updates state correctly for successful guess',()=>{
+            
+        })
+    })
+    describe('some guessed words',()=>{
+        test('updates state correctly for unsuccessful guess',()=>{
+
+        })
+        test('updates state correctly for successful guess',()=>{
+            
+        })
+    })
+})
