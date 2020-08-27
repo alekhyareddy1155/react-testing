568336e81aa6d20f7133f359e96f51da4ba39fa8
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-27 17:24:25 +0530

    useReducer setup

--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { mount} from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
+import hookActions from './actions/hookActions';
 
-const setup=()=>{
-  return shallow(<App />);
+const mockGetSecretWord = jest.fn();
+
+/**
+ * Setup function for app component.
+ * @param {string} secretWord - desired secretWord state value for test
+ * @returns {ReactWrapper}
+ */
+const setup = () => {
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // const mockUseReducer = jest.fn()
+  //   .mockReturnValue([
+  //     { secretWord, language: 'en' },
+  //     jest.fn()
+  //   ]);
+
+  // React.useReducer = mockUseReducer;
+
+  // use mount, because useEffect not called on `shallow`
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 }
+
 
 test('App renders without error',()=>{
   const wrapper=setup();
   const component=findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+})
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 })
