a5e1af14c9ae8f006ca7866b24c7b74a6a490835
Reddy <Reddy.Alekhya@go-mmt.com>
2020-08-27 14:34:53 +0530

    state controlled input field test

--- base-for-hooks-context/src/Input.js
+++ base-for-hooks-context/src/Input.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 function Input({secretWord}) {
+  const [currentGuess, setCurrentGuess]=React.useState("");
   return (
     <div data-test="component-input">
-      
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button 
+          data-test="submit-button"
+          onClick={(evt) => {
+            evt.preventDefault();
+            // // update guessedWords
+            // const letterMatchCount = getLetterMatchCount(currentGuess, secretWord);
+            // const newGuessedWords = [...guessedWords, { guessedWord: currentGuess, letterMatchCount }];
+            // setGuessedWords(newGuessedWords);
+
+            // // check against secretWord and update success if needed
+            // if (currentGuess === secretWord) {
+            //   setSuccess(true);
+            // }
+            // // clear input box
+            // setCurrentGuess("");
+          }}
+          className="btn btn-primary mb-2">
+          {/* {stringsModule.getStringByLanguage(language, 'submit')} */}
+          submit
+        </button>
+      </form>
     </div>
   )
 }
